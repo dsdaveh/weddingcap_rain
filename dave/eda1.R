2d8866f3537344fb24d6eaba3d50ed848b11e385
Dave Hurst <dhurst79@gmail.com>
2015-10-06 19:09:28 -0700

    use generic path

--- dave/eda1.R
+++ dave/eda1.R
@@ -1,4 +1,25 @@
 library(data.table)
+library(Metrics)
+library(dplyr)
 
-train <- fread("~/GitHub/weddingcap_rain/train.csv")
+train <- fread("../train.csv")
 summary(train)
+
+train[, median(Expected)]
+t0b <- proc.time()
+train[,mean(Expected),Id][,median(V1)]
+t1b <- proc.time()
+t1b-t0b
+
+#mae <- function( y, yhat) mean( abs( y-yhat ))
+
+train.df <- tbl_df(train)
+t0 <- proc.time()
+train %>% group_by(Id) %>% summarise( V1 = mean(Expected)) %>% summarise( median(V1))
+t1 <- proc.time()
+t1-t0
+
+xx <- train %>% 
+    select( starts_with("Ref"), starts_with("Rho"), starts_with("Zdr"), starts_with("Kdp") ) %>%
+    transmute( allNA = all(is.na(Ref), is.na(Kdp)))
+transmute( allNA = any(! is.na(Ref)))
