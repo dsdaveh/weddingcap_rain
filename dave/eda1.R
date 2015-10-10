2b66e92eea51b61216eb3964e237510f3dacad4c
David Allen Hurst <Dave.A.Hurst@seagate.com>
2015-10-10 09:06:04 -0700

    plots

--- dave/eda1.R
+++ dave/eda1.R
@@ -19,7 +19,29 @@ rm(refs)
 rhos <- train %>%
   select( starts_with("Rho") ) %>%
   gather( Rhoxxx, Value, starts_with("Rho")) %>%
-  group_by(Rhoxxx)
+  group_by(Rhoxxx)rm
 
 rhos %>% ggvis( ~Value) %>%
   layer_densities( fill = ~Rhoxxx, density_args = list( na.rm=TRUE))
+
+rm(rhos)
+
+zdrs <- train %>%
+  select( starts_with("Zdr") ) %>%
+  gather( Zdrxxx, Value, starts_with("Zdr")) %>%
+  group_by(Zdrxxx)
+
+kdps %>% ggvis( ~Value) %>%
+  layer_densities( fill = ~Zdrxxx, density_args = list( na.rm=TRUE))
+
+rm(kdps)
+
+kdps <- train %>%
+  select( starts_with("Kdp") ) %>%
+  gather( Kdpxxx, Value, starts_with("Kdp")) %>%
+  group_by(Kdpxxx)
+
+zdrs %>% ggvis( ~Value) %>%
+  layer_densities( fill = ~Kdpxxx, density_args = list( na.rm=TRUE))
+
+rm(zdrs)
