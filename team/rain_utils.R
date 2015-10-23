36e734bb9ced2f9bee05b404a9bde94e19e4fd6c
scottmark <scottemark@gmail.com>
2015-10-22 22:20:04 -0500

    added duration function to utils and include when loading train

--- team/rain_utils.R
+++ team/rain_utils.R
@@ -27,6 +27,27 @@ tcheck <- function(t=1) {
     }
 }
 
+duration <- function(minutes_past) {
+  
+  # calculate the length of time for which each reflectivity value is valid
+  valid_time <- rep(0, length(minutes_past))
+  valid_time[1] <- minutes_past[1]
+  if (length(valid_time) > 1) {
+    for (i in seq(2, length(minutes_past))) {
+      valid_time[i] <- minutes_past[i] - minutes_past[i-1]
+    }
+    valid_time[length(valid_time)] = valid_time[length(valid_time)] + 60 - sum(valid_time)
+  } else {
+    # if only 1 observation, make it valid for the entire hour
+    valid_time <- 60
+  }
+  
+  valid_time = valid_time / 60
+  
+  return(valid_time)
+  
+}
+
 mpalmer <- function(ref, minutes_past) {
     
     # order reflectivity values and minutes_past
