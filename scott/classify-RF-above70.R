db7e862fa02daf4f7a9bea40bdd0af19e1e89d65
scottmark <scottemark@gmail.com>
2015-10-25 21:51:54 -0500

    WIP additional work towards classifying above 70

--- scott/classify-RF-above70.R
+++ scott/classify-RF-above70.R
@@ -13,7 +13,7 @@ library(rattle)
 source("../team/rain_utils.R")
 source("../team/data-prep.R")
 
-# collapse rows using means across; might consider adding MP
+# collapse rows using means across
 forest.dt <- train[ , .(
   radardist_km = max(radardist_km, na.rm=TRUE)
   ,records = .N
@@ -38,48 +38,79 @@ forest.dt <- train[ , .(
   ,kdp.50thmean = weighted.mean(Kdp_5x5_50th, duration, na.rm=TRUE)
   ,kdp.90thmean = weighted.mean(Kdp_5x5_90th, duration, na.rm=TRUE)
   ,Measured = max(Expected)
+  ,m.palmer=mpalmer(Ref, minutes_past)
 ), by=Id] 
 
 
 # set the classes
-forest.dt$above70 <- ifelse(forest.dt$Measured > 70, 1, 0)
-forest.dt$above40 <- ifelse(forest.dt$Measured > 40, 1, 0)
+forest.dt$above70 <- ifelse(forest.dt$Measured > 70, 1, 0) # 28,370
+forest.dt$above40 <- ifelse(forest.dt$Measured > 40, 1, 0) # 33,711
+forest.dt$above70 <- as.factor(forest.dt$above70)
+forest.dt$above40 <- as.factor(forest.dt$above40)
 
 #save if interested in returning to this point
 save(forest.dt, file="../forest-dt.RData")
 #load("../forest-dt.RData")
+
+# get a random sample of 28,730 Ids below 70 for a 50-50 split to train
+set.seed(498)
+Ids.samplebelow70 <- sample(forest.dt[forest.dt$above70==0,]$Id, 28370)
+treetrain <- rbind(forest.dt[Ids.samplebelow70,], forest.dt[forest.dt$above70==1,])
 
 # formula
 n <- names(forest.dt)
 form70 <- as.formula(paste("above70 ~", paste(n[!n %in% c("above70", "above40", "Id", "Measured")], collapse = " + ")))
 form40 <- as.formula(paste("above40 ~", paste(n[!n %in% c("above70", "above40", "Id", "Measured")], collapse = " + ")))
 
-# train a basic tree with all vars - can't predict the above70s
-treemodel.all <- rpart(formula=form40, data=forest.dt, method="class")
+###  
+# works 53% and lots of false positives, bad for MAE 
+# train a basic tree to predict above 40
+treemodel.all <- rpart(formula=form40, data=treetrain, method="class")
 #fancyRpartPlot(treemodel.all)
 #treemodel.all
-treemodel.pred.train <- predict(treemodel.all, forest.dt, type="class")
+treemodel.pred.train <- predict(treemodel.all, newdata=forest.dt, type="class")
 treemodel.confusion.train <- table(observed=forest.dt$above40, predicted=treemodel.pred.train )
 treemodel.confusion.train
+sum(diag(treemodel.confusion.train)) / sum(treemodel.confusion.train) # 0.5372025
 
 # try just refs
-treemodel.refs <- rpart(formula=above40~ref.mean+ref.10thmean+ref.50thmean+ref.90thmean+refcomp.mean+refcomp.10thmean+refcomp.50thmean+refcomp.90thmean,
+treemodel.refs <- rpart(formula=above40~ref.mean+ref.10thmean+ref.50thmean+ref.90thmean
+                        +refcomp.mean+refcomp.10thmean+refcomp.50thmean+refcomp.90thmean
+                        +radardist_km+records,
                         data=forest.dt, method="class")
 #fancyRpartPlot(treemodel.all)
 #treemodel.all
-treemodel.refs.pred.train <- predict(treemodel.refs, forest.dt, type="class")
-treemodel.refs.confusion.train <- table(observed=forest.dt$above40, predicted=treemodel.refs.pred.train )
+treemodel.refs.pred.train <- predict(treemodel.refs, treetrain, type="class")
+treemodel.refs.confusion.train <- table(observed=treetrain$above40, predicted=treemodel.refs.pred.train )
 treemodel.refs.confusion.train
 
+###
+# random forest works 92.7% when it assigns a value, but predicts 401693 NAs!!
+# need to come back to this after imputing
+set.seed(498)
+system.time(rfmodel.allorig <- randomForest(form40, data=treetrain, na.action=na.roughfix, importance=TRUE, ntree=200))
 
-# didn't take time to let this finish - has trouble with fixing the NAs?
-# set.seed(498)
-# system.time(rfmodel.allorig <- randomForest(form, data=forest.dt, na.action=na.roughfix, ntree=20))
-# print(rfmodel.allorig)
-# summary(rfmodel.allorig)
-# round(importance(rfmodel.allorig), 2)
-# plot(rfmodel.allorig, log='y')
-# par(mfrow=c(1,1))
-# varImp(rfmodel.allorig)
-# varImpPlot(rfmodel.allorig)
+rfmodel.allorig.pred <- predict(rfmodel.allorig, newdata=forest.dt, type="response")
+rfmodel.allorig.confusion <- table(observed=forest.dt$above40, predicted=rfmodel.allorig.pred)
+rfmodel.allorig.confusion
+sum(diag(rfmodel.allorig.confusion)) / sum(rfmodel.allorig.confusion) # 0.9273136
+
+print(rfmodel.allorig)
+summary(rfmodel.allorig)
+round(importance(rfmodel.allorig), 2)
+plot(rfmodel.allorig, log='y')
+par(mfrow=c(1,1))
+varImp(rfmodel.allorig)
+varImpPlot(rfmodel.allorig)
+
+###
+# logistic regression
+# same (?) 401693 NAs
+logit <- glm(form40, data = treetrain, family = "binomial")
+logit.pred <- predict(logit, newdata = forest.dt, type = "response")
+logit.pred.class <- round(logit.pred)
+logit.confusion <- table(observed=forest.dt$above40, predicted=logit.pred.class)
+logit.confusion
+sum(diag(logit.confusion)) / sum(logit.confusion) # 0.7694106
+
 
