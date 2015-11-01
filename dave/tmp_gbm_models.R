ec8d1b22ce66d7f9d06abeb84200eb33ac9928e5
Dave Hurst <dhurst79@gmail.com>
2015-10-31 19:23:00 -0700

    gbm models run script

---
+++ dave/tmp_gbm_models.R
@@ -0,0 +1,18 @@
+rdata_file <- "train_10pct.RData"
+source("KS_gbm_cv.R")  #KS_gbm_cv_OUT_f07_10pct_mpnew.txt
+
+#run replacing new_mpalmer with generic
+gitsource("KS_gbm_cv.R")   #KS_gbm_cv_OUT_f07_10pct_mpgen.txt
+
+#run after adding kdp
+rm(chg_mpalmer)
+source("KS_gbm_cv.R")   #KS_gbm_cv_OUT_f07_10pct_mpkdp.txt
+
+rdata_file <- "train_full.RData"
+source("KS_gbm_cv.R")   #KS_gbm_cv_OUT_f07_full_mpkdp.txt
+
+cv_frac_trn <- 1
+create_submission <- TRUE
+source("KS_gbm_cv.R")   #KS_gbm_cv_OUT_f10_full_mpkdp.txt
+
+
