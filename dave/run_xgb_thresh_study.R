83268b8b5be5a65400fb7d71362fe15c619ebc94
David Allen Hurst <Dave.A.Hurst@seagate.com>
2015-11-11 20:58:55 -0800

    run thresh lower and upper 0 100 by 10

--- dave/run_xgb_thresh_study.R
+++ dave/run_xgb_thresh_study.R
@@ -2,7 +2,7 @@ library (ggvis)
 library (tidyr)
 rdata_file <- '../train_agg_10pct.RData'
 
-create_submission <- FALSE
+create_submission <- TRUE
 cv_frac_trn <- .7
 tcheck.print <- TRUE
 
@@ -10,8 +10,10 @@ mae_res <- data.frame()
 run_time <- numeric() 
 
 
-for (set_seed in c(1999, 2015, 7)) {
-    for (set_rain_thresh in seq(10, 80, 10)) {
+for (set_seed in c(1999)) {   #}, 2015, 7)) {
+    for (set_rain_thresh in seq(10, 100, 10)) {
+        set_rain_thresh_lower <- set_rain_thresh - 10
+        run_id <- sprintf("gbm_slice_%d_%d", set_rain_thresh_lower, set_rain_thresh)
         source ('../dave/gbm_cv.R')
         mae_res <- rbind( mae_res, data.frame( seed=set_seed, rain_thresh=set_rain_thresh, mae_cv_test, mae_cv_trn, mae_xgb))
         run_time <- c(run_time, sum( time_df$delta ))
